8a2d8d95c5a51747550f67ee18917c1571ffbd82
a.melendezreyes <alejandra@preevix.com>
2018-06-07 10:20:10 -0600

    restServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -5,3 +5,21 @@
 //============
 
 process.env.PORT = process.env.PORT || 3000;
+
+//==========
+//Entorno
+//============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==========
+//Base  de datos
+//============
+
+let urlDB;
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe'
+}else{
+    urlDB = 'mongodb://cafe-user:abc123456@ds151970.mlab.com:51970/cafe'
+}
+
+process.env.URLDB = urlDB;
